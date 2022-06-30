982ac7b88aba238bcdc14c3d244dbaee9cdf6554
yujinkimkimkim <kimyj4852@naver.com>
2022-06-30 20:53:46 +0900

    동아리 과제

---
+++ 2weeks/cal/c.js
@@ -0,0 +1,5 @@
+function add(){
+    var a = Number(document.getElementById("aa").value);
+    var b = Number(document.getElementById("bb").value);
+    document.getElementById("result").value = a + b;
+}
---
+++ 2weeks/static/dd.js
@@ -0,0 +1,45 @@
+var computer=document.getElementById("computer");
+
+computer.addEventListener('click', function() {
+    alert('computer');
+
+});
+
+var writing=document.getElementById("writing");
+writing.addEventListener('click', function() {
+    alert('writing');
+
+});
+
+var CEsw=document.getElementById("CEsw");
+CEsw.addEventListener('click', function() {
+    alert('CEsw');
+
+});
+
+var CElr=document.getElementById("CElr");
+CElr.addEventListener('click', function() {
+    alert('CElr');
+
+});
+
+var cpro=document.getElementById("cpro");
+cpro.addEventListener('click', function() {
+    alert('cprogramming');
+
+});
+
+var math=document.getElementById("math");
+math.addEventListener('click', function() {
+    alert('math');
+
+});
+
+var cul=document.getElementById("cul");
+cul.addEventListener('click', function() {
+    alert('cul');
+
+});
+
+//alert 괄호 안에 '' 안에 그 아이디 이름 쓰고
+// .textcontent 하면 그 칸에 있는 값 불러와짐
